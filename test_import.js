45582910644f66cf2373795ca1dccd39220fbd95
duart38 <mr.codercuracao@gmail.com>
2020-09-04 22:46:17 +0200

    Added nsted logic to import test file

--- test_import.js
+++ test_import.js
@@ -1,6 +1,13 @@
+import { cleanWorkerImports } from "./utils.js";
+import Observe from "https://raw.githubusercontent.com/duart38/Observe/master/Observe.bundle.js";
+import {add} from "./sandbox/impone.js";
+
 export function CallMe(){
     console.log("I've been imported!!!!")
 }
 export function returnNumber(){
     return 1;
+}
+export function nestedTest(){
+    add(1, 1)
 }
